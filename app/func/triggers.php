58752924ed595e1ae69539e33a6d196a076302d3
Brent Leavitt <brent@trainingdoulas.com>
2022-12-12 12:09:33 -0600

    Adding basic trigger setup

--- app/func/triggers.php
+++ app/func/triggers.php
@@ -17,12 +17,44 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 //SETUP TRIGGERS
 $nb_triggers = [
-    '(trigger_name)' => [ '(action_hooks)' ],
-    '(trigger_name)' => [ '(action_hooks)' ],
+    'ASSIGNMENT_SUBMITTED',
+    'ASSIGNMENT_RESUBMITTED',
+    'ASSIGNMENT_INCOMPLETE',
+    'ASSIGNMENT_GRADED',
+    'TRAINER_NEW_STUDENT',
+    'TRAINER_REASSIGNMENT',
+    'NEW_STUDENT_COMMENT',
+    'NEW_TRAINER_COMMENT',
+    'NEW_STUDENT_REGISTRATION',
+    'NEW_STUDENT_VERIFICATION',
+    'PASSWORD_RESET', 
+    //what else? 
+]; 
 
-]  
+function nb_make_triggers_listen( $triggers )
+{
 
+        foreach( $triggers as $t )
+        {
+            $trigger = new $t();
+            $trigger->listen(); 
+        }
+} 
 
+function nb_activate_triggers(){
+    global $nb_triggers; 
+  
+    if( !get_option( 'nb_notes_trigger_templates' ) )
+    {
+        //array of triggers set with empty arrays set as a holding bay for indexing of trigger templates (Nb_Notes_Templates CPTs)
+        $base_triggers_array = []; 
+        foreach( $nb_triggers as $trigger )
+            $base_triggers_array[ $trigger ] = []; 
 
+        add_option( 'nb_notes_trigger_templates', $base_triggers_array ); 
+    }
+}
+
+add_action( 'nb_notes_activate', 'Nb_Notes\App\Func\nb_activate_triggers' ); 
 
 ?>
